26a08558f78029f86fd1efda96a998b7a71a0d01
Jainarayan Singh <88608543+Jainarayan-Singh@users.noreply.github.com>
2025-09-14 19:18:28 +0530

    Update latex_editor.py

--- latex_editor.py
+++ latex_editor.py
@@ -4,4 +4,10 @@ latex_bp = Blueprint('latex_editor', __name__, url_prefix='/admin')
 
 @latex_bp.route('/latex_editor')
 def latex_editor():
-    return render_template('latex_editor.html')
+    
+    from admin import admin_required
+    @admin_required
+    def _inner():
+        return render_template('latex_editor.html')
+
+    return _inner()
